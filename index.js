7800189de84c2a512ad5da1489596bd1be10a866
Henrique Yuji Rossetti Inonhe <henriqueinonhe@gmail.com>
2023-09-24 18:04:59 -0300

    Setup basic server

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 import Fastify from "fastify";
+import staticMiddleware from "@fastify/static";
+import { resolve } from "node:path";
+import { readFile } from "node:fs/promises";
 
-const fastify = Fastify({});
+const fastify = Fastify({
+  logger: true,
+});
 
 fastify.get("/", async (req, res) => {
-  res.send("Ok");
+  const index = await readFile("./public/index.html", "utf-8");
+  res.headers({
+    "Content-Type": "text/html",
+  });
+  res.send(index);
+});
+
+fastify.register(staticMiddleware, {
+  root: resolve(new URL(import.meta.url).pathname, "../public"),
 });
 
 await fastify.listen({ port: 3000 });
